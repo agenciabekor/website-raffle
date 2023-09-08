187849cfe918f919a2a314109ff6447a5a59ca27
Alexandre Bekor <alexandre@agenciabekor.com>
2023-09-08 11:42:46 -0300

    change logo

--- pages/_app.js
+++ pages/_app.js
@@ -7,14 +7,14 @@ function MyApp({ Component, pageProps }) {
     <div className="flex flex-col min-h-screen">
       <header className="flex px-4 pt-8 justify-center bg-gray-800">
         <div className="flex">
-          <Image src={Logo} height={60} width={210} />
+          <h1 className='text-lg text-white font-bold'>Copa Graça</h1>
         </div>
       </header>
       <main className="flex flex-grow items-center justify-center bg-gray-800">
         <Component {...pageProps} />
       </main>
       <footer className="py-1 text-center bg-black text-white">
-        <p className="text-sm">Feito por Agência Bekor &copy; 2021</p>
+        <p className="text-sm">Feito por Agência Bekor &copy; 2023</p>
       </footer>
     </div>
   )
